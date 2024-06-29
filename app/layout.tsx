0d3967f0050ffd86f429bcdc2548a1275d2f5c1a
alazaradane <alazargithub369@gmail.com>
2024-06-29 20:34:23 +0300

    add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,46 @@
 import React from 'react'
-import {
-  ClerkProvider,
-} from '@clerk/nextjs'
+import { ClerkProvider,} from '@clerk/nextjs'
 import './globals.css'
+// eslint-disable-next-line camelcase
+import {Inter, Space_Grotesk} from 'next/font/google'
+import type { Metadata } from 'next'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100','200','300','400','500','600','700','800','900' ],
+  variable: '--font-inter',
+})
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300','400','500','600','700' ],
+  variable: '--font-spaceGrotesk',
+})
+
+
+export const metadata: Metadata = {
+  title: 'DevFlow',
+  description: "A community-driven platform for asking and answering programming questions. Get help share knowledge, and collaborate with developers from around the world. Explore topics in web development, algorithms, data structures, and more. ",
+  icons: {
+    icon: '/assets/images/site-logo.svg'
+  }
+  
+}
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      elements:{
+        formButtonPrimary:'primary-gradient',
+        footerActionLink:'primary-text-gradient hover:text-primary-500'
+      }
+    }}>
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className=' h1-bold'>Check text</h1>
           {children}
         </body>
       </html>
