5fec2def7faced8418c4c33cacd48cf51fbffd69
alazaradane <alazargithub369@gmail.com>
2024-06-29 17:02:27 +0300

    adding routing to the root and auth

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <main>sign-up</main>
+  )
+}
+
+export default SignUp
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Home() {
-  return (
-    <div>
-      <h1 className=" h1-bold">Nextjs 13 We&apos;re Comming!</h1>
-    </div>
-  );
-}
