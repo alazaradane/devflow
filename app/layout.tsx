7692f6f5db6e752641707bb820b06df6cd10de2b
alazaradane <alazargithub369@gmail.com>
2024-06-30 19:04:54 +0300

    Implment theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 // eslint-disable-next-line camelcase
 import {Inter, Space_Grotesk} from 'next/font/google'
 import type { Metadata } from 'next'
+import { ThemeProvider } from '@/context/ThemeProvider'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -32,18 +33,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider appearance={{
-      elements:{
-        formButtonPrimary:'primary-gradient',
-        footerActionLink:'primary-text-gradient hover:text-primary-500'
-      }
-    }}>
+    
       <html lang="en">
         <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          <h1 className=' h1-bold'>Check text</h1>
-          {children}
+          <ClerkProvider appearance={{
+            elements:{
+              formButtonPrimary:'primary-gradient',
+              footerActionLink:'primary-text-gradient hover:text-primary-500'
+            }
+          }}>
+            <ThemeProvider>
+              {children}
+            </ThemeProvider>
+          </ClerkProvider>
         </body>
       </html>
-    </ClerkProvider>
   )
 }
