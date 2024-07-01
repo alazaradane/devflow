ddce1ab35e8a2427b15799fc3ac0441e6b586268
alazaradane <alazargithub369@gmail.com>
2024-07-01 10:11:11 +0300

    schadcn setup

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable tailwindcss/no-custom-classname */
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
@@ -5,7 +6,7 @@ import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
-    <nav className=' flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12'>
+    <nav className=' flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 dark:shadow-none sm:px-12'>
         <Link href='/' className=' flex items-center gap-1'>
             <Image src='/assets/images/site-logo.svg' alt='devFlow' width={23} height={23}/>
             <p className=' h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden'>Dev <span className=' text-primary-500'>Overflow</span></p>
