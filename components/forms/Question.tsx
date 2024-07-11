dba9c2f415c3ed7a0dcd6f1d3550a92eae96d783
alazaradane <alazargithub369@gmail.com>
2024-07-11 17:25:35 +0300

    Adding remove functionality for tags

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-return-assign */
 "use client"
-import { useRef } from 'react';
+import React, { useRef } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -17,6 +17,9 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { QuestionsSchema } from "@/lib/validations"
+import { Badge } from '../ui/badge';
+import Image from 'next/image';
+
 
 const Question = () => {
   const editorRef = useRef(null);
@@ -32,9 +35,38 @@ const Question = () => {
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof QuestionsSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values);
+  }
+
+  const handleInputkeyDown = (e: React.KeyboardEvent<HTMLInputElement>, field:any)=>{
+      if(e.key==='Enter' && field.name ==='tags'){
+        e.preventDefault();
+
+        const tagInput = e.target as HTMLInputElement
+        const tagValue = tagInput.value.trim()
+
+        if(tagValue !== ''){
+          if(tagValue.length > 15){
+            return form.setError('tags', {
+              type : 'required',
+              message: 'Tag must be less than 15 characters'
+            })
+          }
+
+          if(!field.value.includes(tagValue as never)){
+            form.setValue('tags', [...field.value, tagValue])
+            tagInput.value =''
+            form.clearErrors('tags')
+          }else{
+            form.trigger()
+          }
+        }
+      }
+  }
+
+  const handleRemoveTag = (tag:string, field:any)=>{
+    const newTags = field.value.filter((t:string)=> t !== tag)
+    form.setValue('tags', newTags)
   }
 
   return (
@@ -108,10 +140,34 @@ const Question = () => {
                 Tags <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl className=" mt-3.5">
-                <Input
-                  className=" no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                  placeholder="shadcn" {...field}
-                />
+                <>
+                  <Input
+                    className=" no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                    placeholder="Add tags"
+                    onKeyDown={(e)=>handleInputkeyDown(e, field)}
+                  />
+                  {
+                    field.value.length > 0 && (
+                      <div className=' flex-start mt-2.5 gap-2.5'>
+                        {field.value.map((tag)=>(
+                          <Badge key={tag} 
+                            className='subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-sm border-none px-4 py-2 capitalize'
+                            onClick={()=>handleRemoveTag(tag, field)}
+                          >
+                            {tag}
+                            <Image 
+                              src='/assets/icons/close.svg'
+                              alt='Close icon'
+                              width={12}
+                              height={12}
+                              className=' cursor-pointer object-contain invert-0 dark:invert'
+                            />
+                          </Badge>
+                        ))}
+                      </div>
+                    )
+                  }
+                </>
               </FormControl>
               <FormDescription className=" body-regular mt-2.5 text-light-500">
                 Add up to 3 tags to describe what your question is about. You need to press enter to add a tag
