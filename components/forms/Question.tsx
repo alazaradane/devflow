a8191d58396decdd327a1f7ee9240232067198ca
alazaradane <alazargithub369@gmail.com>
2024-07-11 16:07:18 +0300

    adding tinymc editor

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-return-assign */
 "use client"
-
+import { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
@@ -16,88 +18,112 @@ import {
 import { Input } from "@/components/ui/input"
 import { QuestionsSchema } from "@/lib/validations"
 
-
-
 const Question = () => {
-
+  const editorRef = useRef(null);
 
   const form = useForm<z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
     defaultValues: {
       title: "",
       explanation: "",
-      tags:[]
+      tags: []
     },
-  })
+  });
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof QuestionsSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
 
   return (
     <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className=" flex w-full flex-col gap-10">
-      <FormField
-        control={form.control}
-        name="title"
-        render={({ field }) => (
-          <FormItem className=" flex w-full flex-col ">
-            <FormLabel className="paragraph-semibold text-dark400_light800">Question Title <span className="text-primary-500">*</span></FormLabel>
-            <FormControl className=" mt-3.5">
-              <Input 
-                className=" no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                placeholder="shadcn" {...field} />
-            </FormControl>
-            <FormDescription className=" body-regular mt-2.5 text-light-500">
-              Be specific and imagine you&apos;re asking a question to another person
-            </FormDescription>
-            <FormMessage className=" text-red-500" />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={form.control}
-        name="explanation"
-        render={({ field }) => (
-          <FormItem className=" flex w-full flex-col gap-3 ">
-            <FormLabel className="paragraph-semibold text-dark400_light800">Detailed explanation of your problem <span className="text-primary-500">*</span></FormLabel>
-            <FormControl className=" mt-3.5">
-              <Input 
-                className=" no-focus paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                placeholder="shadcn" {...field} />
-            </FormControl>
-            <FormDescription className=" body-regular mt-2.5 text-light-500">
-              Introduce the problem expand what you put on the title. Minimum 20 characters 
-            </FormDescription>
-            <FormMessage className=" text-red-500" />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={form.control}
-        name="tags"
-        render={({ field }) => (
-          <FormItem className=" flex w-full flex-col ">
-            <FormLabel className="paragraph-semibold text-dark400_light800">Tags<span className="text-primary-500">*</span></FormLabel>
-            <FormControl className=" mt-3.5">
-              <Input 
-                className=" no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                placeholder="shadcn" {...field} />
-            </FormControl>
-            <FormDescription className=" body-regular mt-2.5 text-light-500">
-              Add up to 3 tags to describe what your question is about. You need to press enter to add a tag
-            </FormDescription>
-            <FormMessage className=" text-red-500" />
-          </FormItem>
-        )}
-      />
-      <Button type="submit">Submit</Button>
-    </form>
-  </Form>
-  )
+      <form onSubmit={form.handleSubmit(onSubmit)} className=" flex w-full flex-col gap-10">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className=" flex w-full flex-col ">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Question Title <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className=" mt-3.5">
+                <Input
+                  className=" no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                  placeholder="shadcn" {...field}
+                />
+              </FormControl>
+              <FormDescription className=" body-regular mt-2.5 text-light-500">
+                Be specific and imagine you&apos;re asking a question to another person
+              </FormDescription>
+              <FormMessage className=" text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="explanation"
+          render={({ field }) => (
+            <FormItem className=" flex w-full flex-col gap-3 ">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Detailed explanation of your problem <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className=" mt-3.5">
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(_evt, editor) => {
+                    // @ts-ignore
+                    editorRef.current = editor}}
+                  initialValue=''
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                      'anchor', 'searchreplace', 'visualblocks', 'codesample', 'fullscreen',
+                      'insertdatetime', 'media', 'table', 
+                    ],
+                    toolbar: 'undo redo  | ' +
+                      'codesample | bold italic forecolor | alignleft aligncenter | ' +
+                      'alignright alignjustify | bullist numlist  | ' +
+                      'removeformat | help',
+                    content_style: 'body { font-family:Inter font-size:16px }'
+                  }}
+                />
+              </FormControl>
+              <FormDescription className=" body-regular mt-2.5 text-light-500">
+                Introduce the problem expand what you put on the title. Minimum 20 characters 
+              </FormDescription>
+              <FormMessage className=" text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem className=" flex w-full flex-col ">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Tags <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className=" mt-3.5">
+                <Input
+                  className=" no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                  placeholder="shadcn" {...field}
+                />
+              </FormControl>
+              <FormDescription className=" body-regular mt-2.5 text-light-500">
+                Add up to 3 tags to describe what your question is about. You need to press enter to add a tag
+              </FormDescription>
+              <FormMessage className=" text-red-500" />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
 
-export default Question
+export default Question;
