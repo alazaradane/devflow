896b52aee879e5980c11837426342963c427bdf1
alazaradane <alazargithub369@gmail.com>
2024-07-15 13:30:14 +0300

    fix eslint error

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -54,7 +54,7 @@ export async function deleteUser(param: DeleteUserParams){
             throw new Error('User not found')
         }
 
-        const userQuestionIds = await Question.find({author: user._id}).distinct('_id')
+        // const userQuestionIds = await Question.find({author: user._id}).distinct('_id')
 
         await Question.deleteMany({author:user._id})
 
